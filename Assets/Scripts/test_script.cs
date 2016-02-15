59594f4a58906721b58d2b759c55815c5a63b838
jkk98 <jkk98@msstate.edu>
2016-02-15 15:29:04 -0600

    Testing Branched Scripts
    Creating an initial script on this branch to test.

---
+++ Assets/Scripts/test_script.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// A test script to see how merge conflicts are handled
+
+public class test_script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
