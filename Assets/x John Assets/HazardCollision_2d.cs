cce91f912bfcf3a3b052c93fe6f446b49f61747e
jkk98 <jkk98@msstate.edu>
2016-03-03 23:43:05 -0600

    Knockback capabilities on hazards somewhat working
    Works for basic rigid bodies. Having an issue with it working with a
    character in horizontal direction.

--- Assets/x John Assets/HazardCollision_2d.cs
+++ Assets/x John Assets/HazardCollision_2d.cs
@@ -10,8 +10,8 @@ public class HazardCollision_2d : MonoBehaviour {
 	// possible types such as damage, knockback, death, etc
 	public type_enum type;
 
-	public double damage = 0;
-	public double force = 0;
+	public float damage = 0;
+	public float force = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -22,15 +22,39 @@ public class HazardCollision_2d : MonoBehaviour {
 	void Update () {
 	
 	}
+		
 
-	void OnTriggerEnter2D(Collider2D other){
+	void OnCollisionEnter2D(Collision2D collision){
 		//Debug.Log (type);
+		Rigidbody2D other_rigidbody = collision.collider.GetComponent<Rigidbody2D>();
+
+		// Getting the average angle of contact
+		Vector2 average_normal = new Vector2 (0, 0); 
+		// gets the average of all contact points
+		foreach (ContactPoint2D contact_point in collision.contacts) {
+			average_normal += contact_point.normal;
+		}
+		average_normal *= -1; // reverses direction outwards
+		average_normal.Normalize ();
+
+		//Vector2 normal = collision.contacts[0].normal * -1; // Gets the angle of the contact from first contact point
+		Debug.Log(average_normal);
+
 		switch(type){
 
 		case type_enum.reset:
-			if (other.gameObject.CompareTag ("Player")) {
+			if (collision.gameObject.CompareTag ("Player")) {
 				UnityEngine.SceneManagement.SceneManager.LoadScene ("john_level_scene2");
 			}
+			break;
+
+		// knockback perpendicular to object
+		case type_enum.knockback:
+			// NOTE: Seems to work fine on rigid bodies though seems to come in conflict with character controller in terms 
+			// of horizontal movement. FixedUpdate may have something to do with this
+			other_rigidbody.AddForce (average_normal * force);
+			//other_rigidbody.velocity = average_normal * force; // need to decide if a force or velocity is wanted here
+
 			break;
 		}
 	}
