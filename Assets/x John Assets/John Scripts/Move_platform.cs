837443887c0bf58e4d509f9056667ef97a9fd236
jkk98 <jkk98@msstate.edu>
2016-03-16 18:55:27 -0500

    Still Attempting to Integrate Code
    Still running through some strange issues caused by the camera and
    moving platforms.

---
+++ Assets/x John Assets/John Scripts/Move_platform.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* Takes a platform with a Slider Joint 2D and allows for it to move and back and 
+ * forth between the upper and lower limits. It is recommended that the initial motor speed
+ * is set to a positive number if possible. There must also be a workable difference between the lower and 
+ * upper limits. Also make sure there is a high amount of friction on horizontal platforms and 
+ * be wary of the repercusions from collisions with other rigid bodies.
+*/
+public class Move_platform : MonoBehaviour {
+
+	private SliderJoint2D slider;
+	private JointLimitState2D goal_limit;
+	private JointMotor2D nextMotor;
+
+	void Awake () {
+		slider = GetComponent<SliderJoint2D>();
+	}
+
+	// Use this for initialization
+	void Start () {
+
+		// Starts out by getting the initial goal limit
+		switch (slider.limitState) {
+
+			// Sets the default goal limit to the upper limit if the motor speed is positive
+			// else it sets it to the lower limit
+		case JointLimitState2D.Inactive:
+			
+			if (slider.motor.motorSpeed > 0) {
+				goal_limit = JointLimitState2D.UpperLimit;
+			} else {
+				goal_limit = JointLimitState2D.LowerLimit;
+			}
+				break;
+			
+			case JointLimitState2D.UpperLimit:
+				goal_limit = JointLimitState2D.LowerLimit;
+				break;
+
+			case JointLimitState2D.LowerLimit:
+				goal_limit = JointLimitState2D.UpperLimit;
+				break;
+
+			case JointLimitState2D.EqualLimits:
+				Debug.Log ("Warning: Moving Platform function will not work when the limits are the same");
+				break;
+
+		}
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log(slider.limitState);
+		if (slider.limitState == goal_limit) {
+			//Debug.Log ("Reached the current goal limit.");
+
+			// Reverses the motor's direction
+			// Cannot directly modify the motor values so just replacing it in the meantime
+			nextMotor.motorSpeed = slider.motor.motorSpeed * -1;
+			nextMotor.maxMotorTorque = slider.motor.maxMotorTorque;
+			slider.motor = nextMotor; 
+
+			// Gets the next goal limit
+			switch (slider.limitState) {
+
+				// Checking for the Inactive limit state only as a debugging procedure
+				case JointLimitState2D.Inactive:
+					Debug.Log ("An error may have occured as limit state should not be inactive.");
+					break;
+
+				case JointLimitState2D.UpperLimit:
+					goal_limit = JointLimitState2D.LowerLimit;
+					break;
+
+				case JointLimitState2D.LowerLimit:
+					goal_limit = JointLimitState2D.UpperLimit;
+					break;
+
+			}
+		}
+	}
+}
