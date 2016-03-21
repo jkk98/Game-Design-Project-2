73888454b7e45abc551a6a182ee453fd4be5f400
jkk98 <jkk98@msstate.edu>
2016-03-21 16:47:19 -0500

    More hub world things

---
+++ Assets/x John Assets/John Scripts/LoadSceneCollider.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneCollider : MonoBehaviour {
+	public string sceneName;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D (Collision2D col) {
+		Debug.Log ("Collision with scene collider");
+		if (col.collider.CompareTag ("Player")) {
+			Debug.Log ("Loading scene: " + sceneName);
+			SceneManager.LoadScene (sceneName);
+		}
+	}
+}
