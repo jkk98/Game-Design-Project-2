5f3ec66affa25295a1d7f6e44dd5aacf17e4ce73
jkk98 <jkk98@msstate.edu>
2016-03-20 21:45:03 -0500

    Started on a generic level select script

---
+++ Assets/x John Assets/John Scripts/LoadHub.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadHub : MonoBehaviour {
+
+	public int hub_scene_index = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void load_Hub() {
+		SceneManager.LoadScene (hub_scene_index);
+	}
+}
