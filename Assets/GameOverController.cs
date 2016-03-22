7752d83542bb1ea4a9517357dfa16834665a5a2c
jkk98 <jkk98@msstate.edu>
2016-03-21 19:48:42 -0500

    "Completed" Game Over Screen
    Functionality besides music seems to be working on game over screen

---
+++ Assets/GameOverController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameOverController : MonoBehaviour {
+
+	public int hub_scene_index = 1;
+
+
+	// reset everything for a new game here
+	void Start () {
+		playerProgress.hasRaptorForm = false;
+		playerProgress.hasTRexForm = false;
+		playerProgress.hasTriceraForm = false;
+		playerProgress.lives = 3;
+	}
+
+	// Restarts the game at the hub screne
+	public void Restart(){
+		Debug.Log ("Restarting game");
+		SceneManager.LoadScene (hub_scene_index);
+	}
+		
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
